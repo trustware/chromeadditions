3d6051295f8bf9030cab3da123bdf7bfa406d1ae
Zane Salem <zsalem@umich.edu>
2015-03-28 23:29:43 -0400

    host request format

--- app/webpage_handler.js
+++ app/webpage_handler.js
@@ -1,7 +1,7 @@
 if(document.getElementById("trustwareButton")){
 	console.log("Trustware Detected!")
 
-	document.getElementById("trustwareButton").addEventListener("click", function(){
+	document.getElementById("trustwareForm").addEventListener("submit", function(){
 	    var msg = {"token": document.getElementById('trustwareToken').value,
 	               "url": document.getElementById('trustwareUrl').value}
 
